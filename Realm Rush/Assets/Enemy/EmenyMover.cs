07d99186d13f34c5c47a6d37913e104751592c37
spiegan77 <stephenpiegan@outlook.com>
2022-05-06 06:23:23 -0400

    Target_Enemies

--- Realm Rush/Assets/Enemy/EmenyMover.cs
+++ Realm Rush/Assets/Enemy/EmenyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EmenyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     void Start()
     {
@@ -17,8 +17,19 @@ public class EmenyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+            
         }
     }
 }
